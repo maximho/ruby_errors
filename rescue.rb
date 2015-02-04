a3fcde22180f898a718311d62544c42d6dabab8f
Maximilianoho <maximho20@gmail.com>
2015-02-04 20:12:17 +0100

    handle exceptions

--- rescue.rb
+++ rescue.rb
@@ -6,13 +6,3 @@ rescue
 	puts "You caused an error!"
 end
 
-
-data = ""
-begin
-	<..code to retrieve the contents of a Web page..>
-	data = <..content of Web page..>
-rescue
-	puts "The Web page could not be loaded! Using default data instead."
-	data = <..load data from local file..>
-end
-puts data 
